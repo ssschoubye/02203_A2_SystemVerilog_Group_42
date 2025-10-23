fd3faf2f47dad8ae30b9994ce30510c11b5e4d1f
jaller698 <c.soetorp@gmail.com>
2025-10-23 12:09:45 +0200

    bugfix

--- task2/memory2.sv
+++ task2/memory2.sv
@@ -52,6 +52,9 @@ function automatic ram_type read_pgm_file(string load_file_name, ref int width, 
     $finish;
   end
 
+  // Skip grayscale
+  $fgets(_line, fd);
+
   // Read pixel data and pack into memory words (little-endian)
   for (int i = 0; i < (width * height)/4; i++) begin
     byte pixels [4];

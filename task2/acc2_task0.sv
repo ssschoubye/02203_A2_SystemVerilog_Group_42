f4587675f459e3f2a3f1dda060fd3d1d7d7dede0
Marimusmaximus <107123960+Marimusmaximus@users.noreply.github.com>
2025-10-23 16:57:58 +0200

    Change acc to acc0 and finish task 0

--- task2/acc2_task0.sv
+++ task2/acc2_task0.sv
@@ -1,9 +1,11 @@
 // -----------------------------------------------------------------------------
 //
-//  Title      :  Edge-Detection design project - task 2.
+//  Title      :  Edge-Detection design project - task 0.
 //             :
-//  Developers :  YOUR NAME HERE - s??????@student.dtu.dk
-//             :  YOUR NAME HERE - s??????@student.dtu.dk
+//  Developers :  Christian Søtorp - s195041@student.dtu.dk
+//             :  Lucas Sjøstrøm   - s224742@student.dtu.dk
+//             :  Marilouise Arbøl - s214401@student.dtu.dk
+//             :  Søren Schoubye   - s224657@student.dtu.dk
 //             :
 //  Purpose    :  This design contains an entity for the accelerator that must be build
 //             :  in task two of the Edge Detection design project. It contains an
@@ -19,7 +21,7 @@
 // reset is active high.
 //------------------------------------------------------------------------------
 
-module acc (
+module acc0 (
     input  logic        clk,        // The clock.
     input  logic        reset,      // The reset signal. Active high.
     output logic [15:0] addr,       // Address bus for data (halfword_t).
@@ -35,85 +37,97 @@ module acc (
   // Insert your design here
   // ---------------------------------------------------
 
-typedef enum logic[2:0]{
-    idle, req_data, read, compute, write, done
-} state_t;
+    typedef enum logic[2:0]{
+        idle, req_data, read, compute, write, done
+    } state_t;
 
-byte unsigned reg_a, reg_b, reg_c, reg_d, next_reg_a, next_reg_b, next_reg_c, next_reg_d;
+    // byte unsigned reg_a, reg_b, reg_c, reg_d, next_reg_a, next_reg_b, next_reg_c, next_reg_d;
+    logic[7:0] reg_a, reg_b, reg_c, reg_d, next_reg_a, next_reg_b, next_reg_c, next_reg_d;
+    logic[15:0] address;
 
-state_t state, next_state;
+    state_t state, next_state;
 
-always_comb begin
-    next_state = state;
-    next_reg_a = reg_a;
-    next_reg_b = reg_b;
-    next_reg_c = reg_c;
-    next_reg_d = reg_d;
-    
-    case(state)
-        idle: begin
-            finish = 0;
-            next_reg_a = 0;
-            next_reg_b = 0;
-            next_reg_c = 0;
-            next_reg_d = 0;
-            
-            if(start) next_state = req_data;
-        end
+    always_comb begin
+        next_state = state;
+        next_reg_a = reg_a;
+        next_reg_b = reg_b;
+        next_reg_c = reg_c;
+        next_reg_d = reg_d;
+        addr = address;
         
-        req_data: begin
-            en = 1;
-            we = 0;
-            addr = 0; // FIXME:
+        case(state)
+            idle: begin
+                finish = 0;
+                next_reg_a = 0;
+                next_reg_b = 0;
+                next_reg_c = 0;
+                next_reg_d = 0;
+                address = 0;
+                
+                if(start) next_state = req_data;
+            end
             
-            next_state = read;            
-        end
-        
-        read: begin
-            next_reg_a = dataR[7:0];
-            next_reg_b = dataR[15:8];
-            next_reg_c = dataR[23:16];
-            next_reg_d = dataR[31:24];
+            req_data: begin
+                en = 1;
+                we = 0;
+                // addr = 0; // FIXME:
+                // addr = addrR;
+                
+                next_state = read;            
+            end
             
-            en = 0;
-            next_state = compute;
-        end
-        
-        compute: begin
-            next_reg_a = 255 - reg_a;
-            next_reg_b = 255 - reg_b;
-            next_reg_c = 255 - reg_c;
-            next_reg_d = 255 - reg_d;
-        
-            next_state = write;
-        end
-        write: begin
-            en = 1;
-            we = 1;
-            addr = 25344;
+            read: begin
+                next_reg_a = dataR[7:0];
+                next_reg_b = dataR[15:8];
+                next_reg_c = dataR[23:16];
+                next_reg_d = dataR[31:24];
+                
+                en = 0;
+                next_state = compute;
+            end
             
-            dataW = {reg_d, reg_c, reg_b, reg_a};
-            next_state = done;
-        end
-        done: begin
-            en = 0;
-            we = 0; 
-            finish = 1;
-        end  
-    endcase
-end
+            compute: begin
+                next_reg_a = 255 - reg_a;
+                next_reg_b = 255 - reg_b;
+                next_reg_c = 255 - reg_c;
+                next_reg_d = 255 - reg_d;
+            
+                address = address + 25344;
+                next_state = write;
+            end
 
-always_ff @(posedge clk or posedge reset) begin
-    if(reset)
-        state <= idle;
-    else begin
-        state <= next_state;
-        reg_a <= next_reg_a;
-        reg_b <= next_reg_b;
-        reg_c <= next_reg_c;
-        reg_d <= next_reg_d;
+            write: begin
+                en = 1;
+                we = 1;
+
+                dataW = {reg_d, reg_c, reg_b, reg_a};
+                next_state = done;
+                
+            end
+            done: begin
+                    en = 0;
+                    we = 0; 
+                if(address >= 50687) begin
+                    finish = 1;
+                end else begin
+                    // addr - 25344 + 1 (jump back to the source image, but on next memory line)
+                    address = address - 25343; 
+
+                    next_state = req_data;
+                end
+            end  
+        endcase
     end
-    
-end
 
+    always_ff @(posedge clk or posedge reset) begin
+        if(reset)
+            state <= idle;
+        else begin
+            state <= next_state;
+            reg_a <= next_reg_a;
+            reg_b <= next_reg_b;
+            reg_c <= next_reg_c;
+            reg_d <= next_reg_d;
+        end
+    end
 endmodule

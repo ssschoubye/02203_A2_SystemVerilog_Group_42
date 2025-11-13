7a545ec41312bacff47b4862663a4895c76c052f
Marimusmaximus <107123960+Marimusmaximus@users.noreply.github.com>
2025-11-13 08:30:15 +0100

    Started working on read_comp and write_comp state - NOT DONE

--- task2/acc.sv
+++ task2/acc.sv
@@ -37,7 +37,7 @@ module acc (
   // ---------------------------------------------------
 
     typedef enum logic[3:0]{
-        idle, read_no_comp, read_and_comp, write_and_comp, done
+        idle, read_no_comp, read_comp, write_comp, done
     } state_t;
 
   reg [23:0][7:0] read_reg, next_read_reg;
@@ -54,11 +54,12 @@ module acc (
         case(state)
             idle:
                 if (start) begin
-                    next_state = read_no_comp;
                     en = 1;
                     we = 0;
                     address = 0;
                     pixel_counter = 0;
+
+                    next_state = read_no_comp;
                 end
 
             read_no_comp: begin
@@ -68,48 +69,93 @@ module acc (
                         {next_read_reg[3], next_read_reg[2], next_read_reg[1], next_read_reg[0]} = dataR;
                         pixel_counter = pixel_counter + 1;
                         address = address + 88;
+                        next_state = read_no_comp;
                     end
                     1: begin
                         {next_read_reg[7], next_read_reg[6], next_read_reg[5], next_read_reg[4]} = dataR;
                         pixel_counter = pixel_counter + 1;
                         address = address + 88;
+                        next_state = read_no_comp;
                     end
                     2: begin
                         {next_read_reg[11], next_read_reg[10], next_read_reg[9], next_read_reg[8]} = dataR;
                         pixel_counter = 0;
                         address = address - 175; // 88 * 2 + 1
+
+                        
+                        next_state = read_comp;
                     end
                     default: // For now should not happen
                         assert(0);
                 endcase
             end
-            read_and_comp: begin
+            read_comp: begin
                 // do something for first three pixels
                 case(pixel_counter)
                     0: begin
+                        {next_read_reg[15], next_read_reg[14], next_read_reg[13], next_read_reg[12]} = dataR;
                         // if edge do nothing
+                        write_reg[0] = 255; // FIXME dummy value
+                        
+
+                        address = address + 88; // Have the next adress ready for next read
+
+
 
                     end
 
                     1: begin
+                        {next_read_reg[19], next_read_reg[18], next_read_reg[17], next_read_reg[16]} = dataR;
+                        write_reg[1] = 255; // FIXME dummy value
+                        
+
+                        address = address + 88; // Have the next adress ready for next read
+
                     end
 
                     2: begin
+                        {next_read_reg[23], next_read_reg[22], next_read_reg[21], next_read_reg[20]} = dataR;
+                        write_reg[2] = 255; // FIXME dummy value
+                        
+
+
+                        address = address + 25344; // Have the next adress ready for next write
 
                     end
+                    default: // For now should not happen
+                        assert(0);
                 endcase
             end
 
-            write_and_comp: begin
+            write_comp: begin
                 // comp and then write
-                logic [7:0] result = 0;
+                logic [7:0] result = 255; // FIXME dummy value
+                
 
                 dataW = {result, write_reg[2], write_reg[1], write_reg[0]};
+                we = 1; // Set write-enable to 1 for a write transaction
+                
+                if(address >= 50687) begin
+                    next_state = read_comp;
+                end else begin
+                    next_state = done;
+                
 
             end
 
-            done:
+            done: begin
+                // en = 0;
+                // we = 0;
+
+                // if(address >= 50687) begin
+                //     finish = 1;
+                // end else begin
+                //     // addr - 25344 + 1 (jump back to the source image, but on next memory line)
+                //     address = address - 25343;
+                //     next_state = read_comp;
+                // end
                 finish = 1; // True
+            end
         endcase
     end;
 
